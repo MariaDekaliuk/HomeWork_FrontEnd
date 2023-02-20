07969d23a3607929f78f06c3d316ec3fc4393b71
User <komplektmed@gmail.com>
2023-02-20 19:29:03 +0100

    hw6

--- homework_6_16feb2023/script/script.js
+++ homework_6_16feb2023/script/script.js
@@ -1,124 +1,67 @@
 console.log("_____________1______________")
-console.log("Hello People!")
-const numer = +prompt("Write a number. Let's find out 10% of this number: ")
-console.log("10% of this numer is: " + numer * 0.1)
+/*Напишите цикл for, который выводит консоль 
+каждое второе число от 0 до 10*/
+for (let i1 = 0; i1 <= 10; i1 = i1 + 2) {
+console.log(i1)}
 
 
 console.log("_____________2______________")
-const number1 = +prompt("Let's find the smallest number. Write number1: ")
-const number2 = +prompt("Write number2: ")
-if (number1 > number2) {
-    console.log("This is the smallest number: " + number2)
-} else if (number2 > number1) {
-    console.log("This is the smallest number: " + number1)
-} else(number2 === number1)
-console.log("There is no smallest number. These numbers are equal")
+/*Напишите цикл for, который выводит в консоль  
+все числа от 55 до 20*/
+for (let i2 = 55; i2 >= 20; i2--) {
+console.log(i2)}
+
 
 console.log("_____________3______________")
-const number3 = +prompt("Введи число. Сравним его со 100: ")
-if (number3 > 100) {
-    console.log(`Число больше 100`)
-} else if (number3 < 100) {
-    console.log(`Число меньше 100`)
-} else if (number3 === 100) {
-    console.log(`Число равно 100`)
+/*Дан массив numbers. 
+Вывести в консоль все числа из массива
+const numbers = [3, 5, 11, 2, 8, 1, 6];*/
+
+const numbers = [3, 5, 11, 2, 8, 1, 6]
+
+for (let i3 = 0; i3 < numbers.length; i3++){
+const numer = numbers[i3]    
+if (!isNaN(numbers[i3])) {
+       console.log(numer)
+    }
 }
 
 console.log("_____________4______________")
-const name = prompt("Write your name: ")
-const age = +prompt("Write your age: ")
-if (age >= 18) {
-    console.log(`Hello! ${name}!`)
-} else if (age < 18) {
-    const ageMin = 18 - age;
-    console.log(`Hi! ${name} see you in ${ageMin} years!`)
+/*Сформировать новый массив numberssquared 
+и передать в него все элементы из массива numbers, 
+возведенные в квадрат*/
+const numberssquared = []
+numberssquared.push(numbers[0] ** 2, numbers[1] ** 2, numbers[2] ** 2, 
+    numbers[3] ** 2, numbers[4] ** 2, numbers[5] ** 2, numbers[6] ** 2)
+console.log(numberssquared)
+
+
+console.log("_____________5______________")
+/*Создать переменную lastelem и передать в нее 
+последний элемент из сформированного массива numberssquared 
+(обратиться к элементу массива по индексу)*/
+
+const lestelem2 = numberssquared[numberssquared.length -1]
+console.log(lestelem2)
+
+const lestelem3 = numberssquared.slice(-1)
+console.log(lestelem3) /*получила новый массив с последним элементом*/
+
+const lestelem = numberssquared.pop(6)
+console.log(lestelem)
+
+
+console.log("_____________6______________")
+/*Дан объект user. Используя данные из объекта, 
+сформировать строку в формате: 
+‘User’s name is  . He is  years old’*/
+
+const user = {
+    firstname: "Ivan",
+    lastname: "Ivanov",
+    age: 20, 
+    salary: 500
 }
 
-
-
-
-console.log("__________extra exercise 1*___________")
-/*чем является каждый элемент массива: 
-строкой, числом или ни тем, ни другим (используя функции isNaN и typeof).*/
-
-const people = ["Masha", 10, {level: "A"}, "knows", 50, {level: "B"}, "German", 100, {level: "C"}, NaN]
-let sumOfNumber = 0; 
-// let sum = 0;
-for (let index = 0; index < people.length; index++) {
-     const element = (people[index]);
-     
-     if (isNaN(element) === false && typeof(element) === "number") {
-     console.log("This element " + index + " is: " + typeof(element))
-    
-    } else if (typeof(element) == 'string') {
-        console.log("This element " + index + " is: " + typeof(element));  }
-        else console.log("This element " + index + " is: something other");
-    }
-
-  
-console.log("__________extra exercise 2*___________")
-/*Просуммируйте все числа в массиве и выведите результат в консоли. 
-  Игнорируйте нечисловые элементы.*/
-
-for (let index = 0; index < people.length; index++) {
-    const element = (people[index]);
-    
-     if (isNaN(element) === false && typeof(element) === "number") {
-        sumOfNumber += people[index]
-        console.log(sumOfNumber)
-}  /*не получилось вывести ТОЛЬКО ИТОГОВУЮ сумму трех чисел в массиве... 
-     Вывелась сначала сумма одного, потом двух и потом 
-     трех чисел...?????????????????????????????????????????????????????????????????*/
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//===============================================================================
-
-    // console.log("This element " + index + " is: строка " + isNaN(element));
-    // console.log("The element with index " + index + " is of type:   " + typeof(element));
-
-    // if (typeof element === 'number' && !isNaN(element)) 
-
-//=======================================================================
-
-   
-
-    //     function getType(n){
-    //     if (Number.isNaN(n)) return "NaN";
-    //     if (typeof(n) === "string", typeof(n) === "number", typeof(n) === "object", typeof(n) === "bigint", typeof(n) ==="function" || typeof(n) === "undefined") return typeof(n);
-    //     return "Something else";
-    // }
-   
-    // for (let index = 0; index < people.length; index++){
-    //     console.log(people[index] + " : " + getType(people[index]));
-    //     if (getType(people[index]) === "number")
-    //         sum += people[index];
-    // }
-    // console.log("Sum of number elements of array is: " + sum);
+console.log(`User's name is: ${user.firstname} ${user.lastname}. He is ${user.age} years old.`)
+console.log("User's name is: " + user.firstname + " " + user.lastname + ". He is " + user.age + " years old.")
